dfa57aef5079841333f6ec09aeb7895f7219e60d
xefiry <seguin.jeremy91@gmail.com>
2014-03-30 12:47:20 +0200

    ajout du script configure

---
+++ configure.sh
@@ -0,0 +1,4 @@
+export CATALINA_HOME=/home/seguinj/TheatreM1GCJS/apache-tomcat-6.0.14
+export JAVA_HOME=/usr
+alias start="apache-tomcat-6.0.14/bin/catalina.sh start"
+alias stop="apache-tomcat-6.0.14/bin/catalina.sh stop"
