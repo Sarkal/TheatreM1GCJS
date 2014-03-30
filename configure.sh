afee8d8bff5b28b823c4fa9502411fd29c2cd946
xefiry <seguin.jeremy91@gmail.com>
2014-03-30 17:15:46 +0200

    correction du script configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 export CATALINA_HOME=/home/seguinj/TheatreM1GCJS/apache-tomcat-6.0.14
 export JAVA_HOME=/usr
-alias start="apache-tomcat-6.0.14/bin/catalina.sh start"
-alias stop="apache-tomcat-6.0.14/bin/catalina.sh stop"
+alias start=`pwd`"/apache-tomcat-6.0.14/bin/catalina.sh start"
+alias stop=`pwd`"/apache-tomcat-6.0.14/bin/catalina.sh stop"
