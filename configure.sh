c6ea8b1568287ab46a504775d647987197449703
xefiry <seguin.jeremy91@gmail.com>
2014-03-30 17:34:35 +0200

    YACOC (Yet Another Correction Of Configure.sh)

--- configure.sh
+++ configure.sh
@@ -1,5 +1,6 @@
 export CATALINA_HOME=/home/$USER/TheatreM1GCJS/apache-tomcat-6.0.14
 export JAVA_HOME=/usr
-alias start=`pwd`"/apache-tomcat-6.0.14/bin/catalina.sh start"
-alias stop=`pwd`"/apache-tomcat-6.0.14/bin/catalina.sh stop"
-alias compile=`pwd`"/apache-tomcat-6.0.14/webapps/ROOT/compile.sh"
+alias start="$CATALINA_HOME/bin/catalina.sh start"
+alias stop="$CATALINA_HOME/bin/catalina.sh stop"
+alias compile="$CATALINA_HOME/webapps/ROOT/compile.sh"
+alias restart="stop; start"
