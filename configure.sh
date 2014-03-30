03aa0358d387adbda27eacef6bfbabd88b0259e1
xefiry <seguin.jeremy91@gmail.com>
2014-03-30 17:19:54 +0200

    correction configure.sh et ajout de des commande compile et restart

--- configure.sh
+++ configure.sh
@@ -1,4 +1,5 @@
-export CATALINA_HOME=/home/seguinj/TheatreM1GCJS/apache-tomcat-6.0.14
+export CATALINA_HOME=/home/$USER/TheatreM1GCJS/apache-tomcat-6.0.14
 export JAVA_HOME=/usr
 alias start=`pwd`"/apache-tomcat-6.0.14/bin/catalina.sh start"
 alias stop=`pwd`"/apache-tomcat-6.0.14/bin/catalina.sh stop"
+alias compile=`pwd`"/apache-tomcat-6.0.14/webapps/ROOT/compile.sh"
