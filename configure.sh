780c970a66895e700dcb7a2f078410fd36802b00
Sarkal <cano.gregory@gmail.com>
2014-04-02 13:31:42 +0200

    Fixed your shit

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
-export CATALINA_HOME=/home/$USER/TheatreM1GCJS/apache-tomcat-6.0.14
+export CATALINA_HOME=$PWD/apache-tomcat-6.0.14
 export JAVA_HOME=/usr
 alias start="$CATALINA_HOME/bin/catalina.sh start"
 alias stop="$CATALINA_HOME/bin/catalina.sh stop"
