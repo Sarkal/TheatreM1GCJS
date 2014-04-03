d4bd59e4781fd87d91a44befb09287e29a3cc4b7
xefiry <seguin.jeremy91@gmail.com>
2014-04-03 19:44:26 +0200

    YACOC

--- configure.sh
+++ configure.sh
@@ -2,5 +2,5 @@ export CATALINA_HOME=$PWD/apache-tomcat-6.0.14
 export JAVA_HOME=/usr
 alias start="$CATALINA_HOME/bin/catalina.sh start"
 alias stop="$CATALINA_HOME/bin/catalina.sh stop"
-alias compile="cd $CATALINA_HOME/webapps/ROOT/ && ./compile.sh && cd -"
+alias compile="cd $CATALINA_HOME/webapps/ROOT/; ./compile.sh; cd -"
 alias restart='stop && compile && start'
