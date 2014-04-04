12df2c11b731cbfb880b2dd0ec71eef7ca190941
seguinj <seguin.jeremy91@gmail.com>
2014-04-04 11:24:32 +0200

    ajout des jar dans le depot

---
+++ bin/apache-tomcat-6.0.14/webapps/ROOT/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# -classpath .:classes:lib/Chat.jar
+
+DEST="WEB-INF/classes/"
+LIB=".:../../lib/servlet-api.jar:WEB-INF/lib/jus.util.jar"
+
+javac -d $DEST -sourcepath src -classpath $LIB $CATALINA_HOME/webapps/ROOT/src/NouvelleRepresentationServlet.java
+
+javac -d $DEST -sourcepath src -classpath $LIB $CATALINA_HOME/webapps/ROOT/src/ProgrammeServlet.java
---
+++ bin/configure.sh
@@ -0,0 +1,8 @@
+export CATALINA_HOME=$PWD/apache-tomcat-6.0.14
+export JAVA_HOME=/usr
+alias catalina="$CATALINA_HOME/bin/catalina.sh"
+alias start="catalina start"
+alias stop="catalina stop"
+alias run="catalina run"
+alias compile="cd $CATALINA_HOME/webapps/ROOT/; ./compile.sh; cd -"
+alias restart='stop && compile && start'
