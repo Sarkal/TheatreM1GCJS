a8c974bc461721a7299f6964ae5ec5ba9f07e8cd
xefiry <seguin.jeremy91@gmail.com>
2014-04-06 17:14:42 +0200

    passage de compile.sh vers un Makefile

--- configure.sh
+++ configure.sh
@@ -4,5 +4,5 @@ alias catalina="$CATALINA_HOME/bin/catalina.sh"
 alias start="catalina start"
 alias stop="catalina stop"
 alias run="catalina run"
-alias compile="cd $CATALINA_HOME/webapps/ROOT/; ./compile.sh; cd -"
+alias compile="cd $CATALINA_HOME/webapps/ROOT/; make; cd -"
 alias restart='stop && compile && start'
