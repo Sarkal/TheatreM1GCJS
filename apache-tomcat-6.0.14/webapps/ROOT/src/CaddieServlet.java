f7663e853ce3d7e82e93269769b0d76f9a1d2dcf
xefiry <seguin.jeremy91@gmail.com>
2014-04-07 22:19:08 +0200

    validation du caddie à debugger

--- apache-tomcat-6.0.14/webapps/ROOT/src/CaddieServlet.java
+++ apache-tomcat-6.0.14/webapps/ROOT/src/CaddieServlet.java
@@ -41,7 +41,7 @@ public class CaddieServlet extends HttpServlet {
 	 */
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
-		String nomS, numS, dateRep, representation, place, noPlace, noRang ,command;
+		String nomS, numS, dateRep, noPlace, noRang ,command;
 		String [] tab;
 		ServletOutputStream out;
 		int nbPlaces = 0;
@@ -91,22 +91,29 @@ public class CaddieServlet extends HttpServlet {
 					ResultSet rs ;
 					
 					if (tab != null && tab[0].equals("del")) {
-						nomS = tab[1];
-						numS = tab[2];
-						dateRep = tab[3];
-						noPlace = tab[4];
-						noRang = tab[5];
-						
-						stmt = c.createStatement();
-						requete = "DELETE FROM LesCaddies where (idClient = " + idClient + 
-						" AND nomS = '" + nomS +"' AND numS = "+ numS +" AND dateRep = TO_DATE('" + dateRep +
-						"', 'yyyy/mm/dd hh24:mi') AND noPlace = "+ noPlace +" AND noRang = "+ noRang +")";
-						rs = stmt.executeQuery(requete);
-						
+						// si on a demandé à supprimer tout le caddie
+						if (tab[1].equals("*")) {
+							stmt = c.createStatement();
+							requete = "DELETE FROM LesCaddies where (idClient = " + idClient +")";
+							stmt.executeQuery(requete);
+						}
+						else {
+							nomS = tab[1];
+							numS = tab[2];
+							dateRep = tab[3];
+							noPlace = tab[4];
+							noRang = tab[5];
+							
+							stmt = c.createStatement();
+							requete = "DELETE FROM LesCaddies where (idClient = " + idClient + 
+							" AND nomS = '" + nomS +"' AND numS = "+ numS +" AND dateRep = TO_DATE('" + dateRep +
+							"', 'yyyy/mm/dd hh24:mi') AND noPlace = "+ noPlace +" AND noRang = "+ noRang +")";
+							stmt.executeQuery(requete);
+							
+							out.println("<p>"+ nomS +" ("+ numS +") - "+ dateRep + " - place"+ 
+									noPlace +" / rang "+ noRang +" Supprimée du caddie</p>");
+						}
 						c.commit();
-						
-						out.println("<p>"+ nomS +" ("+ numS +") - "+ dateRep + " - place"+ 
-								noPlace +" / rang "+ noRang +" Supprimée du caddie</p>");
 					}
 						
 	
@@ -158,6 +165,11 @@ public class CaddieServlet extends HttpServlet {
 				
 				if (nbPlaces != 0) {
 					out.print("<form action=\"CaddieServlet\" method=POST>");
+					out.println("<INPUT type=\"hidden\" value=\"del::*\" name=\"command\">");
+					out.println("<input type=submit value=\"Vider le caddie\">");
+					out.println("</form>");
+					
+					out.print("<form action=\"CaddieServlet\" method=POST>");
 					out.println("<INPUT type=\"hidden\" value=\"valider\" name=\"command\">");
 					out.println("<input type=submit value=\"Valider la commande\">");
 					out.println("</form>");
@@ -167,92 +179,115 @@ public class CaddieServlet extends HttpServlet {
 			}
 
 		} else {
-			out.println("<p>TODO MOTHERFUCKER</p>");
-			/*
-			tab = command.split("::");
-			numS = tab[0];
-			dateRep = tab[1];
-			nomS = tab[2];
+			if (tab[0].equals("valider")) {
 			
-			Connection c = null;
-			String noS, noD, prix;
-			int noSerie, noDossier;
-			try {
-				c = BDConnexion.getConnexion("canog", "bd2013");
-				String requete;
-				Statement stmt;
-				ResultSet rs;
-				
-				// on recupere le numero de serie le plus grand pour avoir le suivant
-				stmt = c.createStatement();
-				requete = "select max(noSerie) from lesTickets";
-				rs = stmt.executeQuery(requete);
-				rs.next();
-				noS = rs.getString(1);
-				noSerie = Integer.parseInt(noS);
-				noSerie++;
-				
-				
-				// idem pour le noDossier
-				stmt = c.createStatement();
-				requete = "select max(noDossier) from LesDossiers";
-				rs = stmt.executeQuery(requete);
-				rs.next();
-				noD = rs.getString(1);
-				noDossier = Integer.parseInt(noD);
-				noDossier++;
-				
-				// on recupere le pris de la place voulue
-				stmt = c.createStatement();
-				requete = "select prix from LesCategories where nomC in (" +
-						"select nomC from LesZones where numZ in (" +
-						"select numZ from lesPlaces where (noPlace = "+ noPlace +" AND noRang = "+ noRang +") ))";
-				rs = stmt.executeQuery(requete);
-				rs.next();
-				prix = rs.getString(1);
-				
-				
-				// on ajoute le dossier dans la base
-				stmt = c.createStatement();
-				requete = "INSERT INTO LesDossiers VALUES ('" + 
-						noDossier +"', '"+ prix +"')";
-				stmt.executeQuery(requete);
-				
-				
-				// on ajoute le nouveau ticket dans la base
-				stmt = c.createStatement();
-				requete = "INSERT INTO LesTickets VALUES ('" + noSerie +
-						"', '"+ numS +"', TO_DATE('" + dateRep +
-						"', 'yyyy/mm/dd hh24:mi'), '"+ noPlace +"', '"+ noRang +"', " +
-						"SYSDATE, '"+ noDossier +"')";
-				stmt.executeQuery(requete);
-				
-				
-				stmt = c.createStatement();
-				requete = "";
-				stmt.executeQuery(requete);
-				c.commit();
-				
-			} catch (NullPointerException e){
-				out.println("Null pointer exception, check connection");
-				out.println(e.getMessage());
-			} catch (ExceptionConnexion e) {
-				out.println("<p>Echec requete </p>");
-				out.println(e.getMessage());
-			} catch (SQLException e) {
-				IO.println("SQLException");
-				out.println(e.getMessage());
-			} finally {
-				if (c != null)
-					try {
-						c.close();
-					} catch (SQLException e) {
-						IO.println("SQLException");
+				Connection c = null;
+				String noS, noD;
+				int noSerie, noDossier, prixTotal = 0;
+				try {
+					c = BDConnexion.getConnexion("canog", "bd2013");
+					String requete;
+					Statement stmt;
+					ResultSet rs, rs2;
+					
+					c.setAutoCommit(false);
+					
+					// on recupere le numero de serie le plus grand pour avoir le suivant
+					stmt = c.createStatement();
+					requete = "select max(noSerie) from lesTickets";
+					rs = stmt.executeQuery(requete);
+					rs.next();
+					noS = rs.getString(1);
+					noSerie = Integer.parseInt(noS);
+					noSerie++;
+					
+					
+					// idem pour le noDossier
+					stmt = c.createStatement();
+					requete = "select max(noDossier) from LesDossiers";
+					rs = stmt.executeQuery(requete);
+					rs.next();
+					noD = rs.getString(1);
+					noDossier = Integer.parseInt(noD);
+					noDossier++;
+					
+					// pour chaque place dans le caddie
+					stmt = c.createStatement();
+					requete = "select numS, nomS, TO_CHAR(dateRep, "+ format +
+							"), noPlace, noRang from LesCaddies " +
+							"where idClient = " + idClient;
+					rs = stmt.executeQuery(requete);
+					
+					while (rs.next()) {
+						nbPlaces++;
+						numS = rs.getString(1);
+						nomS = rs.getString(2);
+						dateRep = rs.getString(3);
+						noPlace = rs.getString(4);
+						noRang = rs.getString(5);
+						
+						// on recupere le prix de la place voulue
+						stmt = c.createStatement();
+						requete = "select prix from LesCategories where nomC in (" +
+								"select nomC from LesZones where numZ in (" +
+								"select numZ from lesPlaces where (noPlace = "+ noPlace +" AND noRang = "+ noRang +") ))";
+						rs2 = stmt.executeQuery(requete);
+						rs2.next();
+						prixTotal += Integer.parseInt(rs2.getString(1));
+						
+						out.println("<p>Avant ticket </p>");
+						
+						// on ajoute le nouveau ticket dans la base
+						stmt = c.createStatement();
+						requete = "INSERT INTO LesTickets VALUES ('" + noSerie +
+								"', '"+ numS +"', TO_DATE('" + dateRep +
+								"', 'yyyy/mm/dd hh24:mi'), '"+ noPlace +"', '"+ noRang +"', " +
+								"SYSDATE, '"+ noDossier +"')";
+						stmt.executeQuery(requete);
+						
+						out.println("<p>Apres ticket </p>");
 					}
+					out.println("</table>");
+					
+					// si on a un prix different de 0
+					if (prixTotal != 0) {
+						// on ajoute le dossier dans la base
+						stmt = c.createStatement();
+						requete = "INSERT INTO LesDossiers VALUES ('" + 
+								noDossier +"', '"+ prixTotal +"')";
+						stmt.executeQuery(requete);
+					}
+					
+					// on supprime le caddie du client de la base
+					stmt = c.createStatement();
+					requete = "DELETE FROM LesCaddies where (idClient = " + idClient +")";
+					stmt.executeQuery(requete);
+					
+					out.println("<p>Avant commit </p>");
+					
+					c.commit();
+					
+					out.println("<p>Places commandées, prix à payer : "+ prixTotal +"</p>");
+					
+				} catch (NullPointerException e){
+					out.println("Null pointer exception, check connection");
+					out.println(e.getMessage());
+				} catch (ExceptionConnexion e) {
+					out.println("<p>Echec requete </p>");
+					out.println(e.getMessage());
+				} catch (SQLException e) {
+					IO.println("SQLException");
+					out.println(e.getMessage());
+				} finally {
+					if (c != null)
+						try {
+							c.close();
+						} catch (SQLException e) {
+							IO.println("SQLException");
+						}
+				}
 			}
-			*/
 		}
-
 		out.println("<hr><p><font color=\"#FFFFFF\"><a href=\"/index.html\">Page d'accueil</a></p>");
 		out.println("</BODY>");
 		out.close();
