d0179d445af792cf8411667826c18fa8dd27cf0f
sarkal <cano.gregory@gmail.com>
2014-04-08 12:56:36 +0200

    test sql

--- rel_solution.sql
+++ rel_solution.sql
@@ -45,7 +45,7 @@ create table LesTickets (noSerie number (4), numS number (4),
                  references LesPlaces (noPlace,noRang),
       constraint tickets_c5 check (dateEmission <= dateRep),
       constraint tickets_c6 foreign key (noDossier)  
-                 references LesDossiers (noDossier) deferrable
+                 references LesDossiers (noDossier) deferrable initially deferred
 );
 
 create table LesCaddies (idClient number, nomS varchar2(40), numS number (4),
